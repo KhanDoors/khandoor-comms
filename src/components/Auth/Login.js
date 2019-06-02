59a89403b46bb66fc836ccdd6dadd89b87a8c9be
syyad khan <syyadkhan@yahoo.com>
2019-06-02 11:56:33 -0400

    logo, header and login form

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,7 +1,32 @@
 import React from "react";
 
 function Login(props) {
-  return <div>Login</div>;
+  const [login, setLogin] = React.useState(true);
+
+  return (
+    <div>
+      <h2 className="mv3">{login ? "Login" : "Create Account"}</h2>
+      <form className="flex flex-column">
+        {!login && (
+          <input type="text" placeholder="Your Name" autoComplete="off" />
+        )}
+        <input type="email" placeholder="Email Address" autoComplete="off" />
+        <input type="password" placeholder="Secure Password Please" />
+        <div className="flex mt3">
+          <button type="submit" className="button pointer mr2">
+            Submit
+          </button>
+          <button
+            type="button"
+            className="button pointer"
+            onClick={() => setLogin(prevLogin => !prevLogin)}
+          >
+            {login ? "Create an Account" : "Login Page"}
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default Login;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,37 @@
 import React from "react";
+import { withRouter, NavLink } from "react-router-dom";
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <div className="header">
+      <div className="flex">
+        <img src="/logo1.jpg" alt="Khandoor Comms" className="logo" />
+        <NavLink to="/" className="header-title">
+          Khandoor Comms
+        </NavLink>
+        <NavLink to="/" className="header-link">
+          New
+        </NavLink>
+        <div className="divider">|</div>
+        <NavLink to="/top" className="header-link">
+          Top
+        </NavLink>
+        <div className="divider">|</div>
+        <NavLink to="/search" className="header-link">
+          Search
+        </NavLink>
+        <div className="divider">|</div>
+        <NavLink to="/create" className="header-link">
+          Submit
+        </NavLink>
+      </div>
+      <div className="flex">
+        <NavLink to="/login" className="header-link">
+          Login
+        </NavLink>
+      </div>
+    </div>
+  );
 }
 
-export default Header;
+export default withRouter(Header);
