0236cf92ff671d60b45f68e63751596de44981ce
syyad khan <syyadkhan@yahoo.com>
2019-06-03 13:39:51 -0400

    persisttent form submission

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,17 +1,47 @@
 import React from "react";
+import useFormValidation from "./useFormValidation";
+
+const INITIAL_STATE = {
+  name: "",
+  email: "",
+  password: ""
+};
 
 function Login(props) {
+  const { handleSubmit, handleChange, values } = useFormValidation(
+    INITIAL_STATE
+  );
   const [login, setLogin] = React.useState(true);
 
   return (
     <div>
       <h2 className="mv3">{login ? "Login" : "Create Account"}</h2>
-      <form className="flex flex-column">
+      <form onSubmit={handleSubmit} className="flex flex-column">
         {!login && (
-          <input type="text" placeholder="Your Name" autoComplete="off" />
+          <input
+            onChange={handleChange}
+            value={values.name}
+            name="name"
+            type="text"
+            placeholder="Your Name"
+            autoComplete="off"
+          />
         )}
-        <input type="email" placeholder="Email Address" autoComplete="off" />
-        <input type="password" placeholder="Secure Password Please" />
+        <input
+          onChange={handleChange}
+          value={values.email}
+          name="email"
+          type="email"
+          placeholder="Email Address"
+          autoComplete="off"
+        />
+        <input
+          onChange={handleChange}
+          value={values.password}
+          name="password"
+          type="password"
+          placeholder="Secure Password Please"
+        />
         <div className="flex mt3">
           <button type="submit" className="button pointer mr2">
             Submit
